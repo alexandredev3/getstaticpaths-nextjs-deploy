a2a1798f5ce07d724ca5c1f2a033b7a34e9eb35e
alexandredev3 <costasalexandre511@gmail.com>
2021-02-16 16:01:08 +0000

    Initial commit

---
+++ pages/users/[id].tsx
@@ -0,0 +1,56 @@
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next'
+import api from '../../src/services/api';
+
+type User = {
+  data: {
+    id: number;
+    avatar_url: string;
+    name: string;
+    bio: string;
+    public_repos: string;
+    followers: string;
+    following: string;
+  }
+}
+
+export default function User({ data }: User) {
+  return (
+    <div>
+      <img src={data.avatar_url} alt="Avatar Github" width={200}/>
+      <h1>{data.name}</h1>
+      <h2>{data.bio}</h2>
+      <span>Repositorios: {data.public_repos} - Followers: {data.followers} - Following: {data.following}</span>
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async (context) => {
+  return {
+    paths: [
+      {
+        params: {
+          id: 'alexandredev3'
+        }
+      },
+      {
+        params: {
+          id: 'facebook'
+        }
+      }
+    ],
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const id = context.params?.id as string;
+
+  const response = await api.get(`/users/${id}`);
+
+  return {
+    props: {
+      id,
+      data: response.data,
+    }
+  }
+}
